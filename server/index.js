79b17364b1defe7890c0fe461e8b94bcd8e768be
ssumanlife <ksm9805@naver.com>
2024-08-20 20:30:01 +0900

    feat: mongoDB연동 및 서버세팅 #8

---
+++ server/index.js
@@ -0,0 +1,35 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import dotenv from 'dotenv'
+import cors from 'cors'
+import { MongoClient } from 'mongodb'
+const app = express()
+
+app.use(cors())
+app.use(bodyParser.json())
+dotenv.config()
+const PORT = process.env.PORT || 3000
+const MONGOURL = process.env.MONGO_URL
+
+const sampleDBName = 'sample_mflix'
+let database
+
+async function connectToDB() {
+	try {
+		const client = await MongoClient.connect(MONGOURL)
+		database = client.db(sampleDBName)
+		app.listen(PORT, () => console.log(`MongoDB listening on ${PORT}`))
+	} catch (error) {
+		res.status(500).send(error)
+	}
+}
+connectToDB()
+
+app.get('/api/users', async (req, res) => {
+	try {
+		const users = await database.collection('users').find({}).toArray()
+		res.send(users)
+	} catch (error) {
+		res.status(500).send(error)
+	}
+})
