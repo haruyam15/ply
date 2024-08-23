89ce3a1b7c8cf956d703c9044c1d570b72a7646d
Haeun <haruyam15@gmail.com>
2024-08-23 19:10:28 +0900

    feat: 라우팅 셋팅 (#4)
    Home, Timeline, Playlist, Like, Profile 페이지

--- src/hooks/useResponsivNav.ts
+++ src/hooks/useResponsivNav.ts
@@ -16,10 +16,9 @@ const useResponsiveNav = () => {
 		}, 300)
 
 		handleResize()
-		// 이벤트 리스너 등록
+
 		window.addEventListener('resize', handleResize)
 
-		// 컴포넌트 언마운트 시 이벤트 리스너 정리
 		return () => {
 			window.removeEventListener('resize', handleResize)
 		}
--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,24 +1,35 @@
-import styled from '@emotion/styled'
 import Topbar from '@/layout/topbar/Topbar'
 import Navbar from '@/layout/nav/Navbar'
 import Logo from '@/components/Logo'
 import useResponsiveNav from '@/hooks/useResponsivNav'
-import Container from '@/layout/Container'
+import useNavStore from '@/stores/useNavStore'
+import styled from '@emotion/styled'
+import { Outlet } from 'react-router-dom'
+
 function Layout() {
+	const isExpand = useNavStore((state) => state.isExpand)
 	useResponsiveNav()
 	return (
 		<Wrapper className="wrap">
 			<Topbar />
 			<Navbar />
 			<Logo />
-			<Container />
+			<Container isExpand={isExpand}>
+				<Outlet />
+			</Container>
 		</Wrapper>
 	)
 }
-
 export default Layout
 
 const Wrapper = styled.div`
 	min-height: 100vh;
 	min-width: 950px;
+`
+
+const Container = styled.div<{ isExpand: boolean }>`
+	padding-top: 60px;
+	padding-left: ${({ isExpand }) => (isExpand ? '240px' : '78px')};
+	min-height: 100vh;
+	box-sizing: border-box;
 `
