f78f275823d6d75f98cfcb25d71eb5a6fa1940c8
dhkim511 <ddhkim11@gmail.com>
2024-08-25 16:53:23 +0900

    feature: 회원정보 수정 api (#37)

--- server/index.js
+++ server/index.js
@@ -1,147 +1,205 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import { MongoClient } from 'mongodb';
+import express from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
+import { MongoClient } from 'mongodb'
 
-const app = express();
+const app = express()
 
-app.use(cors());
-app.use(express.json());
-dotenv.config();
+app.use(cors())
+app.use(express.json())
+dotenv.config()
 
-const PORT = process.env.PORT || 3000;
-const MONGOURL = process.env.MONGO_URL;
+const PORT = process.env.PORT || 3000
+const MONGOURL = process.env.MONGO_URL
 
-const sampleDBName = 'hansarang3club';
-let database;
+const sampleDBName = 'hansarang3club'
+let database
 
 async function connectToDB() {
-  try {
-    const client = await MongoClient.connect(MONGOURL);
-    database = client.db(sampleDBName);
-    app.listen(PORT, () => console.log(`MongoDB listening on ${PORT}`));
-  } catch (error) {
-    console.error(error);
-  }
+	try {
+		const client = await MongoClient.connect(MONGOURL)
+		database = client.db(sampleDBName)
+		app.listen(PORT, () => console.log(`MongoDB listening on ${PORT}`))
+	} catch (error) {
+		console.error(error)
+	}
 }
-connectToDB();
+connectToDB()
 
 app.get('/api/login', async (req, res) => {
-  const { userid, password } = req.query;
-  try {
-    const user = await database.collection('information').findOne({ userid, password });
-    if (user) {
-      res.status(200).send({ message: 'Login successful', user });
-    } else {
-      res.status(401).send({ message: 'Invalid userid or password' });
-    }
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
+	const { userid, password } = req.query
+	try {
+		const user = await database.collection('information').findOne({ userid, password })
+		if (user) {
+			res.status(200).send({ message: 'Login successful', user })
+		} else {
+			res.status(401).send({ message: 'Invalid userid or password' })
+		}
+	} catch (error) {
+		res.status(500).send(error)
+	}
+})
 // 로그인 성공 curl -X GET "http://localhost:8080/api/login?userid=johndoe&password=john1234"
 // 로그인 실패 curl -X GET "http://localhost:8080/api/login?userid=invaliduser&password=invalidpass"
 
 app.post('/api/register', async (req, res) => {
-  const { userid, password, profileimage, nickname } = req.body;
-  try {
-    const newUser = {
-      userid,
-      password,
-      profileimage,
-      nickname
-    };
-    const result = await database.collection('information').insertOne(newUser);
-    res.status(201).send({ message: 'User registered successfully', userId: result.insertedId });
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
+	const { userid, password, profileimage, nickname } = req.body
+	try {
+		const newUser = {
+			userid,
+			password,
+			profileimage,
+			nickname,
+		}
+		const result = await database.collection('information').insertOne(newUser)
+		res.status(201).send({ message: 'User registered successfully', userId: result.insertedId })
+	} catch (error) {
+		res.status(500).send(error)
+	}
+})
 // curl -X POST "http://localhost:8080/api/register" -H "Content-Type: application/json" -d '{"userid":"johndoe","password":"john1234","profileimage":"","nickname":"Johnny"}' ( 회원가입 )
 
 app.get('/api/followers', async (req, res) => {
-  const { userid } = req.query;
-  try {
-    const user = await database.collection('users').findOne({ "information.userid": userid });
-    if (user) {
-      res.status(200).send({ followers: user.followers });
-    } else {
-      res.status(404).send({ message: 'User not found' });
-    }
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
+	const { userid } = req.query
+	try {
+		const user = await database.collection('users').findOne({ 'information.userid': userid })
+		if (user) {
+			res.status(200).send({ followers: user.followers })
+		} else {
+			res.status(404).send({ message: 'User not found' })
+		}
+	} catch (error) {
+		res.status(500).send(error)
+	}
+})
 //curl -X GET "http://localhost:8080/api/followers?userid=johndoe" ( 해당 유저의 팔로워 목록 )
 
 app.get('/api/following', async (req, res) => {
-  const { userid } = req.query;
-  try {
-    const user = await database.collection('users').findOne({ "information.userid": userid });
-    if (user) {
-      res.status(200).send({ following: user.following });
-    } else {
-      res.status(404).send({ message: 'User not found' });
-    }
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
+	const { userid } = req.query
+	try {
+		const user = await database.collection('users').findOne({ 'information.userid': userid })
+		if (user) {
+			res.status(200).send({ following: user.following })
+		} else {
+			res.status(404).send({ message: 'User not found' })
+		}
+	} catch (error) {
+		res.status(500).send(error)
+	}
+})
 // curl -X GET "http://localhost:8080/api/following?userid=johndoe" ( 해당 유저가 팔로잉하는 유저들 )
 
 app.get('/api/likes', async (req, res) => {
-  const { userid } = req.query;
-  try {
-    const user = await database.collection('users').findOne({ "information.userid": userid });
-    if (user) {
-      res.status(200).send({ likes: user.like });
-    } else {
-      res.status(404).send({ message: 'User not found' });
-    }
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
+	const { userid } = req.query
+	try {
+		const user = await database.collection('users').findOne({ 'information.userid': userid })
+		if (user) {
+			res.status(200).send({ likes: user.like })
+		} else {
+			res.status(404).send({ message: 'User not found' })
+		}
+	} catch (error) {
+		res.status(500).send(error)
+	}
+})
 // curl -X GET "http://localhost:8080/api/likes?userid=johndoe" ( 해당 유저가 좋아요한 플리 )
 
-
 app.get('/api/myPlayList', async (req, res) => {
-  const { userid } = req.query;
-  try {
-    const user = await database.collection('users').findOne({ "information.userid": userid });
-    if (user) {
-      res.status(200).send({ myPlayList: user.myPlayList });
-    } else {
-      res.status(404).send({ message: 'User not found' });
-    }
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
+	const { userid } = req.query
+	try {
+		const user = await database.collection('users').findOne({ 'information.userid': userid })
+		if (user) {
+			res.status(200).send({ myPlayList: user.myPlayList })
+		} else {
+			res.status(404).send({ message: 'User not found' })
+		}
+	} catch (error) {
+		res.status(500).send(error)
+	}
+})
 // curl -X GET "http://localhost:8080/api/myPlayList?userid=johndoe" ( 해당 유저가 만든 플레이리스트 )
 
 app.get('/api/playlist', async (req, res) => {
-  const { title } = req.query;
-  try {
-    const playlist = await database.collection('playListData').findOne({ title });
-    if (playlist) {
-      res.status(200).send(playlist);
-    } else {
-      res.status(404).send({ message: 'Playlist not found' });
-    }
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
+	const { title } = req.query
+	try {
+		const playlist = await database.collection('playListData').findOne({ title })
+		if (playlist) {
+			res.status(200).send(playlist)
+		} else {
+			res.status(404).send({ message: 'Playlist not found' })
+		}
+	} catch (error) {
+		res.status(500).send(error)
+	}
+})
 // curl -X GET "http://localhost:8080/api/playlist?title=Playlist%201" ( 플레이 리스트 상세 )
 
 app.get('/api/playlists/ids', async (req, res) => {
-  try {
-    const playlists = await database.collection('playListData').find({}, { projection: { _id: 1 } }).toArray();
-    res.status(200).send(playlists);
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
+	try {
+		const playlists = await database
+			.collection('playListData')
+			.find({}, { projection: { _id: 1 } })
+			.toArray()
+		res.status(200).send(playlists)
+	} catch (error) {
+		res.status(500).send(error)
+	}
+})
 // curl -X GET "http://localhost:8080/api/playlists/ids" ( 모든 플리 탐색에서 씀 )
 
+app.post('/api/update-profile', async (req, res) => {
+	const { userid, newUserid, password, profileimage, nickname } = req.body
+
+	try {
+		// 기존 사용자 확인
+		const user = await database.collection('information').findOne({ userid })
+		if (!user) {
+			return res.status(404).send({ message: 'User not found' })
+		}
+
+		// newUserid가 이미 존재하는지 확인
+		if (newUserid && newUserid !== userid) {
+			const existingUser = await database.collection('information').findOne({ userid: newUserid })
+			if (existingUser) {
+				return res.status(409).send({ message: 'New userId already exists' })
+			}
+		}
+
+		// 사용자 정보 업데이트 (userId 변경 포함)
+		const updatedUser = {
+			userid: newUserid || userid, // 새로운 userId가 있으면 변경, 없으면 기존 userId 유지
+			...(password && { password }),
+			...(profileimage && { profileimage }),
+			...(nickname && { nickname }),
+		}
+
+		// userId가 변경된 경우 처리
+		if (newUserid && newUserid !== userid) {
+			// 새 userId로 새로운 문서를 생성하고, 기존 문서 삭제
+			await database.collection('information').insertOne(updatedUser)
+			await database.collection('information').deleteOne({ userid })
+
+			// users 컬렉션에서도 동일하게 처리
+			const userDocument = await database
+				.collection('users')
+				.findOne({ 'information.userid': userid })
+			if (userDocument) {
+				userDocument.information = updatedUser
+				userDocument.id = newUserid
+				await database.collection('users').insertOne(userDocument)
+				await database.collection('users').deleteOne({ 'information.userid': userid })
+			}
+		} else {
+			// userId가 변경되지 않은 경우, 기존 문서만 업데이트
+			await database.collection('information').updateOne({ userid }, { $set: updatedUser })
+
+			await database
+				.collection('users')
+				.updateOne({ 'information.userid': userid }, { $set: { information: updatedUser } })
+		}
+
+		res.status(200).send({ message: 'User profile updated successfully' })
+	} catch (error) {
+		res.status(500).send({ message: 'Failed to update user profile', error })
+	}
+})
