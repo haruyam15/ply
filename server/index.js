971ef79e0b4b6bbce40001d9b87d07185b150187
love1ace <lovelacedud@gmail.com>
2024-08-25 16:17:34 +0900

    feature: api 제작 (#37)

--- server/index.js
+++ server/index.js
@@ -39,3 +39,50 @@ app.get('/api/login', async (req, res) => {
     res.status(500).send(error);
   }
 });
+// 로그인 성공 curl -X GET "http://localhost:8080/api/login?userid=johndoe&password=john1234"
+// 로그인 실패 curl -X GET "http://localhost:8080/api/login?userid=invaliduser&password=invalidpass"
+
+app.get('/api/followers', async (req, res) => {
+  const { userid } = req.query;
+  try {
+    const user = await database.collection('users').findOne({ "information.userid": userid });
+    if (user) {
+      res.status(200).send({ followers: user.followers });
+    } else {
+      res.status(404).send({ message: 'User not found' });
+    }
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+//curl -X GET "http://localhost:8080/api/followers?userid=johndoe"
+
+app.get('/api/following', async (req, res) => {
+  const { userid } = req.query;
+  try {
+    const user = await database.collection('users').findOne({ "information.userid": userid });
+    if (user) {
+      res.status(200).send({ following: user.following });
+    } else {
+      res.status(404).send({ message: 'User not found' });
+    }
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+// curl -X GET "http://localhost:8080/api/following?userid=johndoe"
+
+app.get('/api/likes', async (req, res) => {
+  const { userid } = req.query;
+  try {
+    const user = await database.collection('users').findOne({ "information.userid": userid });
+    if (user) {
+      res.status(200).send({ likes: user.like });
+    } else {
+      res.status(404).send({ message: 'User not found' });
+    }
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+// curl -X GET "http://localhost:8080/api/likes?userid=johndoe"
