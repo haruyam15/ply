ef2c13f8779fc9ee47d6f695173cda1242fd7dc4
love1ace <lovelacedud@gmail.com>
2024-08-25 16:07:51 +0900

    feature: login api (#37)

--- server/index.js
+++ server/index.js
@@ -1,61 +1,41 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import cors from 'cors'
-import { MongoClient } from 'mongodb'
-const app = express()
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import { MongoClient } from 'mongodb';
 
-app.use(cors())
-app.use(express.json())
-dotenv.config()
-const PORT = process.env.PORT || 3000
-const MONGOURL = process.env.MONGO_URL
+const app = express();
 
-const sampleDBName = 'hansarang3club'
-let database
+app.use(cors());
+app.use(express.json());
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+const MONGOURL = process.env.MONGO_URL;
+
+const sampleDBName = 'hansarang3club';
+let database;
 
 async function connectToDB() {
-	try {
-		const client = await MongoClient.connect(MONGOURL)
-		database = client.db(sampleDBName)
-		app.listen(PORT, () => console.log(`MongoDB listening on ${PORT}`))
-	} catch (error) {
-		res.status(500).send(error)
-	}
+  try {
+    const client = await MongoClient.connect(MONGOURL);
+    database = client.db(sampleDBName);
+    app.listen(PORT, () => console.log(`MongoDB listening on ${PORT}`));
+  } catch (error) {
+    console.error(error);
+  }
 }
-connectToDB()
+connectToDB();
 
-app.get('/api/information', async (req, res) => {
-	try {
-		const information = await database.collection('information').find({}).toArray()
-		res.send(information)
-	} catch (error) {
-		res.status(500).send(error)
-	}
-})
-
-app.get('/api/users', async (req, res) => {
-	try {
-		const users = await database.collection('users').find({}).toArray()
-		res.send(users)
-	} catch (error) {
-		res.status(500).send(error)
-	}
-})
-
-app.get('/api/playListData', async (req, res) => {
-	try {
-		const playListData = await database.collection('playListData').find({}).toArray()
-		res.send(playListData)
-	} catch (error) {
-		res.status(500).send(error)
-	}
-})
-
-app.get('/api/comments', async (req, res) => {
-	try {
-		const comments = await database.collection('comments').find({}).toArray()
-		res.send(comments)
-	} catch (error) {
-		res.status(500).send(error)
-	}
-})
+app.get('/api/login', async (req, res) => {
+  const { userid, password } = req.query;
+  try {
+    const user = await database.collection('information').findOne({ userid, password });
+    if (user) {
+      res.status(200).send({ message: 'Login successful', user });
+    } else {
+      res.status(401).send({ message: 'Invalid userid or password' });
+    }
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
