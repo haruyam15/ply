49f3d87a7c534a523cf4a1342e70bb8f6004176a
ssumanlife <ksm9805@naver.com>
2024-08-27 16:31:16 +0900

    feat: 린트오류수정 (#54)

--- src/components/sign/Signup.tsx
+++ src/components/sign/Signup.tsx
@@ -1,4 +1,9 @@
 /** @jsxImportSource @emotion/react */
+import { useEffect, useRef, useState } from 'react';
+
+import axios from 'axios';
+import { toast, ToastContainer } from 'react-toastify';
+
 import Modal from '@/components/Modal';
 import {
   submitBtn,
@@ -8,9 +13,7 @@ import {
 } from '@/components/sign/Signin';
 import useSignModalStore from '@/stores/useSignModalStore';
 import { colors } from '@/styles/colors';
-import axios from 'axios';
-import { useEffect, useRef, useState } from 'react';
-import { toast, ToastContainer } from 'react-toastify';
+
 import 'react-toastify/dist/ReactToastify.css';
 
 interface SignupData {
@@ -21,11 +24,14 @@ interface SignupData {
 
 const Signup: React.FC = () => {
   const signinModal = useSignModalStore((state) => state.signModals);
-  const closeSigninModal = useSignModalStore((state) => state.closeModal);
   const openSigninModal = useSignModalStore((state) => state.openModal);
-  const [newUser, setNewUser] = useState<SignupData>({nickname: null , userid: null, password: null})
-  const nameRef = useRef<HTMLInputElement>(null)
-  const idRef = useRef<HTMLInputElement>(null)
+  const [newUser, setNewUser] = useState<SignupData>({
+    nickname: null,
+    userid: null,
+    password: null,
+  });
+  const nameRef = useRef<HTMLInputElement>(null);
+  const idRef = useRef<HTMLInputElement>(null);
   const passwordRef = useRef<HTMLInputElement>(null);
 
   const onSignup = (e: React.FormEvent<HTMLFormElement>) => {
@@ -35,53 +41,60 @@ const Signup: React.FC = () => {
       userid: idRef.current?.value ?? null,
       password: passwordRef.current?.value ?? null,
     });
-  }
+  };
 
-  const checkbox = document.getElementById('check') as HTMLInputElement
+  const checkbox = document.getElementById('check') as HTMLInputElement;
+
+  const addNewUser = async () => {
+    if (newUser.userid && newUser.nickname && newUser.password && checkbox?.checked) {
+      try {
+        const res = await axios.post('/api/register', {
+          userid: newUser.userid,
+          nickname: newUser.nickname,
+          password: newUser.password,
+        });
+        if (res.status === 201) {
+          toast('가입이 완료되었습니다.');
+          openSigninModal('signin');
+        }
+      } catch (error) {
+        console.error(error)
+        toast.error('계정 생성 중에 오류가 발생하였습니다. 다시 시도해주세요.');
+      }
+    }
+  };
+
   useEffect(() => {
     const validation = async () => {
-      if(!checkbox?.checked){
-        toast.error('모두 확인하였는지 체크해주세요.')
-        return
+      if (!checkbox?.checked) {
+        toast.error('모두 확인하였는지 체크해주세요.');
+        return;
       }
       try {
         const res = await axios.post('/api/signup/validate', {
           userid: newUser.userid,
           nickname: newUser.nickname,
-        })
-        res.status === 200 ? addNewUser() : null
-      } catch (error: any) {
-        if(error.response && error.response.status === 400){
-          const { field } = error.response.data
-          if(field === 'userid'){
-            toast.error('중복된 ID 입니다.')
-          } else if(field === 'nickname'){
-            toast.error('중복된 닉네임입니다.')
+        });
+        if(res.status === 200){
+          addNewUser()
+        }
+      } catch (error) {
+        if(axios.isAxiosError(error)) {
+          if (error.response && error.response.status === 400) {
+            const { field } = error.response.data;
+            if (field === 'userid') {
+              toast.error('중복된 ID 입니다.');
+            } else if (field === 'nickname') {
+              toast.error('중복된 닉네임입니다.');
+            }
           }
         }
       }
-    }
-    validation()
-  }, [newUser])
+    };
+    validation();
+  }, [newUser, addNewUser]);
 
-  const addNewUser = async () => {
-    if(newUser.userid && newUser.nickname && newUser.password && checkbox?.checked){
-      console.log('----------')
-      try {
-        const res = await axios.post('/api/register',{
-          userid: newUser.userid,
-          nickname: newUser.nickname,
-          password: newUser.password,
-        })
-        if(res.status === 201){
-          toast('가입이 완료되었습니다.')
-          openSigninModal('signin')
-        }
-      } catch (error) {
-        toast.error('계정 생성 중에 오류가 발생하였습니다. 다시 시도해주세요.')
-      }
-    }
-  }
+
 
   const children: React.ReactNode = (
     <>
@@ -100,10 +113,7 @@ const Signup: React.FC = () => {
           <label>Password</label>
         </div>
         <div css={{ fontSize: '12px' }}>
-          <label
-            css={{ cursor: 'pointer', accentColor: `${colors.primaryGreen}` }}
-            htmlFor="check"
-          >
+          <label css={{ cursor: 'pointer', accentColor: `${colors.primaryGreen}` }} htmlFor="check">
             모두 확인 하셨습니까?
             <input css={{ cursor: 'pointer' }} type="checkBox" id="check" />
           </label>
@@ -118,7 +128,13 @@ const Signup: React.FC = () => {
           Go back to login
         </button>
       </p>
-      <ToastContainer position='bottom-center' limit={2} closeButton={false} autoClose={3000} hideProgressBar/>
+      <ToastContainer
+        position="bottom-center"
+        limit={2}
+        closeButton={false}
+        autoClose={3000}
+        hideProgressBar
+      />
     </>
   );
   return (
