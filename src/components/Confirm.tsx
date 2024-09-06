c4fdbadf0a2fca1f80e1a3c6bf0abcf94db52b56
love1ace <lovelacedud@gmail.com>
2024-09-06 17:16:27 +0900

    bug: confirm 오류 해결했습니다 (#160)

--- src/components/Confirm.tsx
+++ src/components/Confirm.tsx
@@ -25,6 +25,17 @@ const Confirm: React.FC<ConfirmDialogProps> = ({ text, onConfirm, onClose, index
         cancelButton: 'custom-cancel-button',
         popup: 'custom-popup',
       },
+      showClass: {
+        popup: 'swal2-show',
+        backdrop: 'swal2-backdrop-show',
+        icon: 'swal2-icon-show',
+      },
+      hideClass: {
+        popup: 'swal2-hide',
+        backdrop: 'swal2-backdrop-hide',
+        icon: 'swal2-icon-hide',
+      },
+      background: '#222',
     }).then((result) => {
       if (result.isConfirmed) {
         onConfirm();
@@ -51,10 +62,10 @@ export const ConfirmStyles = css`
     height: 40px;
     border-radius: 10px;
     margin: 25px 15px 20px 15px;
-    color: #ffffff !important;
-    font-size: 18px !important;
+    color: #ffffff;
+    font-size: 18px;
     font-weight: 500;
-    background-color: #00cc75 !important;
+    background-color: #00cc75;
   }
 
   .custom-cancel-button {
@@ -62,17 +73,43 @@ export const ConfirmStyles = css`
     height: 40px;
     border-radius: 10px;
     margin: 25px 15px 20px 15px;
-    color: #ffffff !important;
-    font-size: 18px !important;
+    color: #ffffff;
+    font-size: 18px;
     font-weight: 500;
   }
 
   .custom-popup {
-    padding: 20px !important;
-    padding-top: 40px !important;
-    background-color: #222 !important; /* 팝업 배경을 어두운 회색으로 변경 */
-    border-radius: 10px !important;
-    color: #ffffff !important;
-    font-size: 18px !important;
+    padding: 20px;
+    padding-top: 40px;
+    background-color: #222;
+    border-radius: 10px;
+    color: #ffffff;
+    font-size: 18px;
+  }
+
+  .swal2-show {
+    animation: fadeIn 0.3s;
+  }
+
+  .swal2-hide {
+    animation: fadeOut 0.3s;
+  }
+
+  @keyframes fadeIn {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
+  }
+
+  @keyframes fadeOut {
+    from {
+      opacity: 1;
+    }
+    to {
+      opacity: 0;
+    }
   }
 `;
