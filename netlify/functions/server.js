f1c6f5764f27eb16330a4a052c3b67e453802b29
Haeun <haruyam15@gmail.com>
2024-09-23 13:43:27 +0900

    Runtime.HandlerNotFound : 핸들러 내보내기

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -4,9 +4,19 @@ async function testConnection() {
   try {
     await mongoose.connect(process.env.MONGO_URL);
     console.log('Connected to MongoDB!');
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: 'Connected to MongoDB!' }),
+    };
   } catch (error) {
     console.error('Connection error:', error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Connection error' }),
+    };
   }
 }
 
-testConnection();
+exports.handler = async (event, context) => {
+  return await testConnection();
+};
