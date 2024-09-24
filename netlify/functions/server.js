e4ef9b15a2120863a04d993af3b34611662d8685
Haeun <haruyam15@gmail.com>
2024-09-24 11:28:25 +0900

    mongo 연결 테스트

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -1,17 +1,18 @@
+const express = require('express');
+const serverless = require('serverless-http');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 // 환경 변수 설정
 dotenv.config();
 
+const app = express();
 const MONGOURL = process.env.MONGO_URL;
 
 async function connectToDB() {
   try {
     // MongoDB에 연결 시도
     await mongoose.connect(MONGOURL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
       connectTimeoutMS: 30000, // 연결 타임아웃 30초
       socketTimeoutMS: 45000, // 소켓 타임아웃 45초
     });
@@ -24,4 +25,11 @@ async function connectToDB() {
   }
 }
 
-connectToDB();
+// 연결 테스트를 위한 라우트 추가
+app.get('/test-connection', async (req, res) => {
+  await connectToDB();
+  res.send('Check the logs for the connection status');
+});
+
+// 서버리스 함수로 변환
+module.exports.handler = serverless(app);
