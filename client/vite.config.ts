2f294eb118ba40f581522fa4d6f372864e4dd68b
Haeun <haruyam15@gmail.com>
2024-10-22 21:52:32 +0900

    deploy: _redirects삭제

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,20 +1,8 @@
-import { viteStaticCopy } from 'vite-plugin-static-copy';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: '_redirects', // _redirects 파일 경로
-          dest: '', // dist 폴더에 복사
-        },
-      ],
-    }),
-  ],
   server: {
     proxy: {
       '/api': {
